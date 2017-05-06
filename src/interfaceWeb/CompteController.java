dad07a8ae4963b2ece3e97a1fdca61dfefc8bb7b
Chris <chrisjahim@free.fr>
2017-05-06 04:16:18 +0200

    Renommage package + ajout package swing

--- src/interfaceWeb/CompteController.java
+++ src/interfaceWeb/CompteController.java
@@ -1,4 +1,4 @@
-package web;
+package interfaceWeb;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
--- src/interfaceWeb/test.java
+++ src/interfaceWeb/test.java
@@ -1,4 +1,4 @@
-package web;
+package interfaceWeb;
 
 public class test
 {
