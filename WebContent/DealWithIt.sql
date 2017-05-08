a9b42af784857a1be46766eabaa6bd08a8a359a2
TGranjon <thomas.granjon@orange.fr>
2017-05-08 21:40:22 +0200

    refactor: Correction d'une erreur d'accès à la BDD

--- WebContent/DealWithIt.sql
+++ WebContent/DealWithIt.sql
@@ -1,162 +1,185 @@
--- MySQL dump 10.13  Distrib 5.7.12, for Win64 (x86_64)
+-- phpMyAdmin SQL Dump
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost    Database: x18a
--- ------------------------------------------------------
--- Server version	5.7.17-log
+-- Client :  127.0.0.1
+-- Généré le :  Lun 08 Mai 2017 à 19:37
+-- Version du serveur :  5.7.14
+-- Version de PHP :  5.6.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Table structure for table `depense`
+-- Base de données :  `dealwithit`
 --
 
-DROP TABLE IF EXISTS `depense`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `depense`
+--
+
 CREATE TABLE `depense` (
   `idUtilisateur` int(11) NOT NULL,
   `idEvenement` int(11) NOT NULL,
-  `date` datetime NOT NULL,
-  `montant` double NOT NULL,
-  `description` varchar(100) COLLATE latin1_general_ci DEFAULT NULL,
-  KEY `fk_idUtilisateur` (`idUtilisateur`),
-  KEY `fk_idEvenement` (`idEvenement`)
+  `date` date NOT NULL,
+  `montant` double NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `depense`
+-- Contenu de la table `depense`
 --
 
-LOCK TABLES `depense` WRITE;
-/*!40000 ALTER TABLE `depense` DISABLE KEYS */;
-/*!40000 ALTER TABLE `depense` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `depense` (`idUtilisateur`, `idEvenement`, `date`, `montant`) VALUES
+(1, 1, '2017-05-08', 200);
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `evenements`
+-- Structure de la table `evenements`
 --
 
-DROP TABLE IF EXISTS `evenements`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `evenements` (
-  `idEvenement` int(11) NOT NULL AUTO_INCREMENT,
+  `idEvenement` int(11) NOT NULL,
   `nomEvenement` varchar(50) COLLATE latin1_general_ci NOT NULL,
-  `budget` double NOT NULL,
-  PRIMARY KEY (`idEvenement`),
-  UNIQUE KEY `idEvenement` (`idEvenement`)
-) ENGINE=MyISAM AUTO_INCREMENT=31 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `budget` double NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 
 --
--- Dumping data for table `evenements`
+-- Contenu de la table `evenements`
 --
 
-LOCK TABLES `evenements` WRITE;
-/*!40000 ALTER TABLE `evenements` DISABLE KEYS */;
-INSERT INTO `evenements` VALUES (1,'Development',1200),(2,'Anniversaire de Jacquie',200),(14,'Mes couilles sur ton front',0),(12,'Lévenement',10),(3,'Flip Party',894),(15,'Lévenement',10),(16,'Lévenement',10),(17,'Lévenement',10),(18,'Lévenement',10),(19,'Lévenement',10),(20,'Lévenement',10),(21,'Lévenement',10),(22,'Lévenement',10),(23,'Lévenement',10),(24,'Lévenement',10),(25,'Lévenement',10),(26,'Lévenement',10),(27,'Lévenement',10),(28,'Lévenement',10),(29,'Lévenement',10),(30,'Lévenement',10);
-/*!40000 ALTER TABLE `evenements` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `evenements` (`idEvenement`, `nomEvenement`, `budget`) VALUES
+(1, 'Development', 1200);
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `participe`
+-- Structure de la table `participe`
 --
 
-DROP TABLE IF EXISTS `participe`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `participe` (
   `idUtilisateur` int(11) NOT NULL,
-  `idEvenement` int(11) NOT NULL,
-  KEY `fk_idUtilisateur` (`idUtilisateur`),
-  KEY `fk_idEvenement` (`idEvenement`)
+  `idEvenement` int(11) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `participe`
+-- Contenu de la table `participe`
 --
 
-LOCK TABLES `participe` WRITE;
-/*!40000 ALTER TABLE `participe` DISABLE KEYS */;
-INSERT INTO `participe` VALUES (1,1),(2,1),(3,1),(4,1),(5,1),(1,2),(2,2),(2,2),(2,2),(2,2),(2,2),(2,2),(2,2),(3,14),(4,14),(2,2),(2,2),(2,2),(2,2),(2,2),(2,2),(2,2),(2,2),(2,2),(2,2),(2,2),(2,2),(2,2),(2,2),(2,2),(2,2);
-/*!40000 ALTER TABLE `participe` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `participe` (`idUtilisateur`, `idEvenement`) VALUES
+(1, 1),
+(2, 1),
+(3, 1),
+(4, 1),
+(5, 1);
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `poste_message`
+-- Structure de la table `poste_message`
 --
 
-DROP TABLE IF EXISTS `poste_message`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `poste_message` (
   `idUtilisateur` int(11) NOT NULL,
   `idEvenement` int(11) NOT NULL,
   `date` date NOT NULL,
-  `message` varchar(200) COLLATE latin1_general_ci NOT NULL,
-  KEY `fk_idUtilisateur` (`idUtilisateur`),
-  KEY `fk_idEvenement` (`idEvenement`)
+  `message` varchar(200) COLLATE latin1_general_ci NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `poste_message`
+-- Contenu de la table `poste_message`
 --
 
-LOCK TABLES `poste_message` WRITE;
-/*!40000 ALTER TABLE `poste_message` DISABLE KEYS */;
-INSERT INTO `poste_message` VALUES (1,1,'2017-05-05','Salut'),(2,2,'2017-05-05','Wesh'),(1,1,'2017-01-01','test'),(1,1,'2017-01-01','test'),(1,1,'2017-01-01','test'),(1,1,'2017-01-01','test'),(1,1,'2017-01-01','test'),(1,1,'2017-01-01','test'),(1,1,'2017-01-01','test');
-/*!40000 ALTER TABLE `poste_message` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `poste_message` (`idUtilisateur`, `idEvenement`, `date`, `message`) VALUES
+(1, 1, '2017-05-08', 'Message de test.');
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `utilisateurs`
+-- Structure de la table `utilisateurs`
 --
 
-DROP TABLE IF EXISTS `utilisateurs`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `utilisateurs` (
-  `idUtilisateur` int(11) NOT NULL AUTO_INCREMENT,
+  `idUtilisateur` int(11) NOT NULL,
   `nom` varchar(50) COLLATE latin1_general_ci NOT NULL,
   `prenom` varchar(50) COLLATE latin1_general_ci NOT NULL,
   `email` varchar(50) COLLATE latin1_general_ci NOT NULL,
-  `pseudo` varchar(20) COLLATE latin1_general_ci NOT NULL,
-  `motDePasse` varchar(30) CHARACTER SET latin1 NOT NULL,
-  PRIMARY KEY (`idUtilisateur`),
-  UNIQUE KEY `idUtilisateur` (`idUtilisateur`),
-  UNIQUE KEY `peudo` (`pseudo`)
-) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `pseudo` varchar(20) COLLATE latin1_general_ci NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 
 --
--- Dumping data for table `utilisateurs`
+-- Contenu de la table `utilisateurs`
 --
 
-LOCK TABLES `utilisateurs` WRITE;
-/*!40000 ALTER TABLE `utilisateurs` DISABLE KEYS */;
-INSERT INTO `utilisateurs` VALUES (1,'Granjon','Thomas','thomas.granjon2@etu.univ-st-etienne.fr','TGranjon','pass'),(2,'Sofonea','Axel','','asofonea','pass'),(3,'Jeamme','Christopher','christopher@jeamme.fr','cjeamme','pass'),(4,'Bruyère','Dimitri','','dbryuere','pass'),(5,'Siracusa','Rémi','','remi42320b','pass'),(6,'Gourdin','Jean-Jacques','Jean-Jacques.Gourdin@rmc.bfmtv.com','Jo Gros Gourdin','FCf(RHçEfz-F(afAD*'),(7,'A','B','C','D','E');
-/*!40000 ALTER TABLE `utilisateurs` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+INSERT INTO `utilisateurs` (`idUtilisateur`, `nom`, `prenom`, `email`, `pseudo`) VALUES
+(1, 'Granjon', 'Thomas', 'thomas.granjon2@etu.univ-st-etienne.fr', 'TGranjon'),
+(2, 'Sofonea', 'Axel', '', 'asofonea'),
+(3, 'Jeamme', 'Christopher', '', 'cjeamme'),
+(4, 'Bruyère', 'Dimitri', '', 'dbryuere'),
+(5, 'Siracusa', 'Rémi', '', 'remi42320b');
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `depense`
+--
+ALTER TABLE `depense`
+  ADD KEY `fk_idUtilisateur` (`idUtilisateur`),
+  ADD KEY `fk_idEvenement` (`idEvenement`);
+
+--
+-- Index pour la table `evenements`
+--
+ALTER TABLE `evenements`
+  ADD PRIMARY KEY (`idEvenement`),
+  ADD UNIQUE KEY `idEvenement` (`idEvenement`);
+
+--
+-- Index pour la table `participe`
+--
+ALTER TABLE `participe`
+  ADD KEY `fk_idUtilisateur` (`idUtilisateur`),
+  ADD KEY `fk_idEvenement` (`idEvenement`);
+
+--
+-- Index pour la table `poste_message`
+--
+ALTER TABLE `poste_message`
+  ADD KEY `fk_idUtilisateur` (`idUtilisateur`),
+  ADD KEY `fk_idEvenement` (`idEvenement`);
+
+--
+-- Index pour la table `utilisateurs`
+--
+ALTER TABLE `utilisateurs`
+  ADD PRIMARY KEY (`idUtilisateur`),
+  ADD UNIQUE KEY `idUtilisateur` (`idUtilisateur`),
+  ADD UNIQUE KEY `peudo` (`pseudo`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `evenements`
+--
+ALTER TABLE `evenements`
+  MODIFY `idEvenement` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT pour la table `utilisateurs`
+--
+ALTER TABLE `utilisateurs`
+  MODIFY `idUtilisateur` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2017-05-08 18:37:34
