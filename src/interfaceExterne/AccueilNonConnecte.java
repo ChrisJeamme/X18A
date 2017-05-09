48fa9a1a25a05a2017a80ab8343a1dd436fa2586
TGranjon <thomas.granjon@orange.fr>
2017-05-09 16:07:02 +0200

    refactor: Redimension de la frame

--- src/interfaceExterne/AccueilNonConnecte.java
+++ src/interfaceExterne/AccueilNonConnecte.java
@@ -10,6 +10,7 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 
 
 public class AccueilNonConnecte {
@@ -22,12 +23,12 @@ public class AccueilNonConnecte {
 		JFrame frame = new JFrame("Deal With It");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setLocation(0, 0);
-		frame.setPreferredSize(new Dimension(740, 1280));
+		frame.setPreferredSize(new Dimension(900, 750));
 		frame.setLayout(new FlowLayout(FlowLayout.CENTER,0,0)); //pour inserer un panel au centre avec 20 pixels de margin H et V
  
 		//Debut entete
 		JPanel panel1 = new JPanel(new BorderLayout(0,0)); //panel principal à inserer 10 pour espacer entre WEST et CENTER, 20 pour espacer entre CENTER et SOUTH
-		panel1.setPreferredSize(new Dimension(720,200));// taille preferee du panel principal
+		panel1.setPreferredSize(new Dimension(880,250));// taille preferee du panel principal
 		frame.add(panel1);
  
 		/****creation des éléments****/
@@ -40,6 +41,7 @@ public class AccueilNonConnecte {
 		lblTitre.setOpaque(true);
 		lblTitre.setBackground(new Color(0,128,255));
 		lblTitre.setText("Deal With It");
+		lblTitre.setHorizontalAlignment(SwingConstants.LEFT);
 		lblTitre.setLocation(100,0);
 		
 		connexion = new JButton("Connexion");
@@ -62,7 +64,7 @@ public class AccueilNonConnecte {
 		
 		//Debut accueil
 		JPanel panel2 = new JPanel(new BorderLayout(10,10)); //panel principal à inserer 10 pour espacer entre WEST et CENTER, 20 pour espacer entre CENTER et SOUTH
-		panel2.setPreferredSize(new Dimension(720,360));// taille preferee du panel principal
+		panel2.setPreferredSize(new Dimension(880,500));// taille preferee du panel principal
 		frame.add(panel2);
 
 		/****creation des éléments****/
@@ -70,44 +72,14 @@ public class AccueilNonConnecte {
 		JLabel labelSexe = new JLabel();
 		labelSexe.setOpaque(true);
 		labelSexe.setBackground(new Color(105,197,6));
+		labelSexe.setText("Presentation");	
+		labelSexe.setHorizontalAlignment(SwingConstants.CENTER);
+		labelSexe.setVerticalAlignment(SwingConstants.CENTER);
 		
 
 		JPanel panel2b = new JPanel(new GridLayout(1,1));//panelWest à l'ouest du borderlayout. c'est une grille d'une colonne avec 4 lignes pour les labels
 		panel2b.add(labelSexe);
-		panel2.add(panel2b,BorderLayout.CENTER);//ajout du panelWest au panel principal
-		
-		
-		//Numéro 3
-		JPanel panel3 = new JPanel(new BorderLayout(10,10)); //panel principal à inserer 10 pour espacer entre WEST et CENTER, 20 pour espacer entre CENTER et SOUTH
-		panel3.setPreferredSize(new Dimension(720,360));// taille preferee du panel principal
-		frame.add(panel3);
-
-		/****creation des éléments****/
-			
-		JLabel labeltruc = new JLabel();
-		labeltruc.setOpaque(true);
-		labeltruc.setBackground(new Color(247,129,216));
-				
-		JPanel panel3b = new JPanel(new GridLayout(1,1));//panelWest à l'ouest du borderlayout. c'est une grille d'une colonne avec 4 lignes pour les labels
-		panel3b.add(labeltruc);
-		panel3.add(panel3b,BorderLayout.CENTER);//ajout du panelWest au panel principal
-		
-		
-		//Numéro 4
-		JPanel panel4 = new JPanel(new BorderLayout(10,10)); //panel principal à inserer 10 pour espacer entre WEST et CENTER, 20 pour espacer entre CENTER et SOUTH
-		panel4.setPreferredSize(new Dimension(720,360));// taille preferee du panel principal
-		frame.add(panel4);
-
-		/****creation des éléments****/
-			
-		JLabel labelmuche = new JLabel();
-		labelmuche.setOpaque(true);
-		labelmuche.setBackground(new Color(64,211,206));
-				
-		JPanel panel4b = new JPanel(new GridLayout(1,1));//panelWest à l'ouest du borderlayout. c'est une grille d'une colonne avec 4 lignes pour les labels
-		panel4b.add(labelmuche);
-		panel4.add(panel4b,BorderLayout.CENTER);//ajout du panelWest au panel principal
-		
+		panel2.add(panel2b,BorderLayout.CENTER);//ajout du panelWest au panel principal		
 		
 		frame.pack();//java propose la meilleure taille en fonction des elements
 		frame.setVisible(true);
