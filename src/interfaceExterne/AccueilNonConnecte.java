679c809a29d0193cd87b85350dfbb92927c1f9dd
remisiracusa <remi.siracusa@etu.univ-st-etienne.fr>
2017-05-09 16:56:52 +0200

    Ajustage des titres et labels de l'interface non connecte.

--- src/interfaceExterne/AccueilNonConnecte.java
+++ src/interfaceExterne/AccueilNonConnecte.java
@@ -19,52 +19,52 @@ public class AccueilNonConnecte {
 		
 	public static void main(String[] args) {
 		
-		//Création de la Frame
-		JFrame frame = new JFrame("Deal With It");
+		JFrame frame = new JFrame();
+		
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setLocation(0, 0);
 		frame.setPreferredSize(new Dimension(900, 750));
-		frame.setLayout(new FlowLayout(FlowLayout.CENTER,0,0)); //pour inserer un panel au centre avec 20 pixels de margin H et V
- 
+		frame.setLayout(new FlowLayout(FlowLayout.CENTER,0,0));
+							
 		//Debut entete
-		JPanel panel1 = new JPanel(new BorderLayout(0,0)); //panel principal à inserer 10 pour espacer entre WEST et CENTER, 20 pour espacer entre CENTER et SOUTH
-		panel1.setPreferredSize(new Dimension(880,250));// taille preferee du panel principal
+		JPanel panel1 = new JPanel(new BorderLayout(0,0));
+		panel1.setPreferredSize(new Dimension(880,200));
 		frame.add(panel1);
- 
+			
 		/****creation des éléments****/
- 
 		JLabel lblMenu = new JLabel();
 		lblMenu.setOpaque(true);
 		lblMenu.setBackground(new Color(0,128,255));		
-		
+		lblMenu.setLocation(500,0);
+							
 		JLabel lblTitre = new JLabel();
 		lblTitre.setOpaque(true);
 		lblTitre.setBackground(new Color(0,128,255));
 		lblTitre.setText("Deal With It");
-		lblTitre.setHorizontalAlignment(SwingConstants.LEFT);
-		lblTitre.setLocation(100,0);
-		
+		lblTitre.setHorizontalAlignment(SwingConstants.CENTER);
+		lblTitre.setVerticalAlignment(SwingConstants.CENTER);
+							
 		connexion = new JButton("Connexion");
 		connexion.setSize(new Dimension(150,30));
 		connexion.setLocation(0, 85);
-		
+					
 		inscription = new JButton("Inscription");
 		inscription.setSize(new Dimension(150,30));
 		inscription.setLocation(170, 85);
-		
+					
 		lblMenu.add(connexion);
 		lblMenu.add(inscription);
-		
-		JPanel panel1b = new JPanel(new GridLayout(1,1));//panelWest à l'ouest du borderlayout. c'est une grille d'une colonne avec 4 lignes pour les labels
+							
+		JPanel panel1b = new JPanel(new GridLayout(1,1));
 		panel1b.add(lblTitre);
 		panel1b.add(lblMenu);
-		panel1.add(panel1b,BorderLayout.CENTER);//ajout du panelWest au panel principal
-		
+		panel1.add(panel1b,BorderLayout.CENTER);
+					
 		// Fin entete
 		
-		//Debut accueil
-		JPanel panel2 = new JPanel(new BorderLayout(10,10)); //panel principal à inserer 10 pour espacer entre WEST et CENTER, 20 pour espacer entre CENTER et SOUTH
-		panel2.setPreferredSize(new Dimension(880,500));// taille preferee du panel principal
+		
+		JPanel panel2 = new JPanel(new BorderLayout(10,10));
+		panel2.setPreferredSize(new Dimension(880,514));
 		frame.add(panel2);
 
 		/****creation des éléments****/
@@ -77,11 +77,11 @@ public class AccueilNonConnecte {
 		labelMain.setVerticalAlignment(SwingConstants.CENTER);
 		
 
-		JPanel panel2b = new JPanel(new GridLayout(1,1));//panelWest à l'ouest du borderlayout. c'est une grille d'une colonne avec 4 lignes pour les labels
+		JPanel panel2b = new JPanel(new GridLayout(1,1));
 		panel2b.add(labelMain);
-		panel2.add(panel2b,BorderLayout.CENTER);//ajout du panelWest au panel principal		
+		panel2.add(panel2b,BorderLayout.CENTER);		
 		
-		frame.pack();//java propose la meilleure taille en fonction des elements
+		frame.pack();
 		frame.setVisible(true);
 	}
 }
