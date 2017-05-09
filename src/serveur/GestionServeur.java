c2c33bd49000934f92a51fbd5e7011fdc0f16954
Chris <chrisjahim@free.fr>
2017-05-09 05:03:49 +0200

    Avancement de GestionServeur (catégorie lecture de xml)

--- src/serveur/GestionServeur.java
+++ src/serveur/GestionServeur.java
@@ -1,6 +1,13 @@
 package serveur;
 
+import javax.swing.text.Utilities;
+
 import bdd.BDD;
+import bdd.InteractionBDD;
+import donnees.Depense;
+import donnees.Evenement;
+import donnees.Message;
+import donnees.Utilisateur;
 import xml.ParserXML;
 import xml.TypeRequete;
 
@@ -20,6 +27,7 @@ public class GestionServeur
 		{
 			//On attend une connexion du client lourd
 			String reception = serveur.attente();
+			reception = ParserXML.clean(reception);
 			System.out.println(reception);
 			
 			//On analyse le type de requet (Envoi/Demande d'objet)
@@ -28,23 +36,24 @@ public class GestionServeur
 			//On traite
 			switch(type)
 			{
-			case DEMANDE_OBJET:
+			case DEMANDE_OBJET:		//Le client lourd nous demande de lui envoyer un objet
 				//ParserXML.analyserDemande(reception);
 				break;
-			case ENVOI_CHAT:
-				
+			case ENVOI_EVENEMENT:	//Le client lourd nous demande de créer cet evenement
+				Evenement evenement = ParserXML.lireEvenement(reception);
+				InteractionBDD.ajoutEvenement(bdd, evenement);
 				break;
-			case ENVOI_EVENEMENT:
-				
+			case ENVOI_DEPENSE:		//Le client lourd nous demande de créer cette dépense
+				Depense depense = ParserXML.lireDepense(reception);
+				InteractionBDD.ajoutDepense(bdd, depense);
 				break;
-			case ENVOI_DEPENSE:
-				
+			case ENVOI_MESSAGE:		//Le client lourd nous demande de créer ce message
+				Message message = ParserXML.lireMessage(reception);
+				InteractionBDD.ajoutMessage(bdd, message);
 				break;
-			case ENVOI_MESSAGE:
-				
-				break;
-			case ENVOI_UTILISATEUR:
-			
+			case ENVOI_UTILISATEUR:	//Le client lourd nous demande de créer cet utilisateur
+				Utilisateur utilisateur = ParserXML.lireUtilisateur(reception);
+				InteractionBDD.ajoutUtilisateur(bdd, utilisateur);
 				break;
 			case INCONNU:
 				System.err.println("Requete recu non reconnue");
