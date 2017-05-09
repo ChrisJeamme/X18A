3b2f642c43a98d921290cd488d513c4c70b20a0b
Chris <chrisjahim@free.fr>
2017-05-09 19:39:50 +0200

    Classe InteractionServeur

--- src/clientExterne/InteractionServeur.java
+++ src/clientExterne/InteractionServeur.java
@@ -2,5 +2,17 @@ package clientExterne;
 
 public class InteractionServeur
 {
-
+	Client client;
+	
+	public InteractionServeur()
+	{
+		client = new Client();
+	}
+	
+	public void envoiServeur(String texte)
+	{
+		client.etablirConnexion();
+    	client.envoyerMessage("<a><b></b></a>");
+    	client.fermetureConnexion();
+	}
 }
