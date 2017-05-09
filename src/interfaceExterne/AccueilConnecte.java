be2f98df04fc75ec960fa48569f724e7a33aee84
remisiracusa <remi.siracusa@etu.univ-st-etienne.fr>
2017-05-09 16:35:06 +0200

    Correction des problèmes liés aux boutons de l'interface externe

--- src/interfaceExterne/AccueilConnecte.java
+++ src/interfaceExterne/AccueilConnecte.java
@@ -22,100 +22,101 @@ public class AccueilConnecte {
 		static JButton operations;
 		
 	public static void main(String[] args) {
-		
-		//Création de la Frame
-		JFrame frame = new JFrame("Deal With It");
+			
+		JFrame frame = new JFrame();
+				
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setLocation(0, 0);
-		frame.setPreferredSize(new Dimension(740, 1280));
+		frame.setPreferredSize(new Dimension(900, 750));
 		frame.setLayout(new FlowLayout(FlowLayout.CENTER,0,0));
- 
+							
 		//Debut entete
-		JPanel haut = new JPanel(new BorderLayout(0,0));
-		haut.setPreferredSize(new Dimension(720,200));
-		frame.add(haut);
- 
-		/****creation des éléments****/ 
- 
+		JPanel panel1 = new JPanel(new BorderLayout(0,0));
+		panel1.setPreferredSize(new Dimension(880,200));
+		frame.add(panel1);
+			
+		/****creation des éléments****/
 		JLabel lblMenu = new JLabel();
 		lblMenu.setOpaque(true);
 		lblMenu.setBackground(new Color(0,128,255));		
-		
+		lblMenu.setLocation(500,0);
+							
 		JLabel lblTitre = new JLabel();
 		lblTitre.setOpaque(true);
 		lblTitre.setBackground(new Color(0,128,255));
 		lblTitre.setText("Deal With It");
-		lblTitre.setLocation(100,0);
-		
+		lblTitre.setLocation(500,0);
+							
 		monCompte = new JButton("Mon compte");
 		monCompte.setSize(new Dimension(150,30));
 		monCompte.setLocation(0, 85);
-		
-		deconnexion = new JButton("Deconnexion");
+					
+		deconnexion = new JButton("Déconnexion");
 		deconnexion.setSize(new Dimension(150,30));
 		deconnexion.setLocation(170, 85);
-		
+					
 		lblMenu.add(monCompte);
 		lblMenu.add(deconnexion);
-		
-		JPanel entete = new JPanel(new GridLayout(1,1));
-		entete.add(lblTitre);
-		entete.add(lblMenu);
-		haut.add(entete,BorderLayout.CENTER);
-		
+							
+		JPanel panel1b = new JPanel(new GridLayout(1,1));
+		panel1b.add(lblTitre);
+		panel1b.add(lblMenu);
+		panel1.add(panel1b,BorderLayout.CENTER);
+					
 		// Fin entete
-		
-		//Debut accueil
-		JPanel centre = new JPanel(new BorderLayout(10,10));
-		centre.setPreferredSize(new Dimension(720,360));
-		frame.add(centre);
-
-		/****creation des éléments****/
-		
-		event = new JButton("Mes Evenements");
+			
+		//Definition des boutons	
+			
+		event = new JButton("event");
 		event.setOpaque(true);
 		event.setBackground(new Color(180,252,103));
-		
-		creerEvent = new JButton("Creer un Evenement");
+					
+		creerEvent = new JButton("creerEvent");
 		creerEvent.setOpaque(true);
 		creerEvent.setBackground(new Color(132,225,33));
-		
-		operations = new JButton("Mes Operations");
+					
+		operations = new JButton("operations");
 		operations.setOpaque(true);
-		operations.setBackground(new Color(105,197,6));		
-
-		JPanel boutonMenu = new JPanel(new GridLayout(1,1));
-		boutonMenu.add(event, BorderLayout.EAST);
-		boutonMenu.add(creerEvent, BorderLayout.CENTER);
-		boutonMenu.add(operations, BorderLayout.WEST);
-		
-		centre.add(boutonMenu,BorderLayout.CENTER);
-		
-		
-		//Numéro 3
-		JPanel panel3 = new JPanel(new BorderLayout(10,10));
-		panel3.setPreferredSize(new Dimension(720,720));
-		frame.add(panel3);
+		operations.setBackground(new Color(105,197,6));
+					
+		//Debut accueil
+		JPanel panel2 = new JPanel(new BorderLayout(10,10));
+		panel2.setPreferredSize(new Dimension(880,250));
+		frame.add(panel2);
 
 		/****creation des éléments****/
-		final JLabel mesEvent = new JLabel("Mes évènements");
-		mesEvent.setBackground(new Color(180,252,103));	
+		JPanel panel2b = new JPanel(new GridLayout(1,1));
+		panel2b.add(event, BorderLayout.EAST);
+		panel2b.add(creerEvent, BorderLayout.CENTER);
+		panel2b.add(operations, BorderLayout.WEST);
+		panel2.add(panel2b,BorderLayout.CENTER);
+					
+		final JLabel mesEvent = new JLabel("event");
 		mesEvent.setOpaque(true);
-		mesEvent.setVisible(false);
-		
-		final JLabel creerEvenement = new JLabel("Créer évènements");
-		creerEvenement.setBackground(new Color(132,225,33));
+		mesEvent.setBackground(new Color(180,252,103));
+		mesEvent.setPreferredSize(new Dimension(880, 264));
+					
+		final JLabel creerEvenement = new JLabel("creerEvent");
 		creerEvenement.setOpaque(true);
-		creerEvenement.setVisible(false);
-		
-		final JLabel mesOperations = new JLabel("Mes opérations");
-		mesOperations.setBackground(new Color(105,197,6));
+		creerEvenement.setBackground(new Color(132,225,33));
+		creerEvenement.setPreferredSize(new Dimension(880, 264));
+					
+		final JLabel mesOperations = new JLabel("operations");
 		mesOperations.setOpaque(true);
-		mesOperations.setVisible(false);
+		mesOperations.setBackground(new Color(105,197,6));
+		mesOperations.setPreferredSize(new Dimension(880, 264));
+					
+		JPanel panel3 = new JPanel();
+		panel3.setPreferredSize(new Dimension(880,264));
+		panel3.add(mesEvent,BorderLayout.CENTER);
+		panel3.add(creerEvenement,BorderLayout.CENTER);
+		panel3.add(mesOperations,BorderLayout.CENTER);
+					
+		frame.add(panel3);
 				
-		panel3.add(mesEvent, BorderLayout.CENTER);
-		panel3.add(creerEvenement, BorderLayout.CENTER);
-		panel3.add(mesOperations, BorderLayout.CENTER);
+		mesEvent.setVisible(false);
+		creerEvenement.setVisible(false);
+		mesOperations.setVisible(false);
 		
 		event.addMouseListener(new MouseAdapter ()
 		{
@@ -136,7 +137,7 @@ public class AccueilConnecte {
 				mesOperations.setVisible(false);
 			}
 		});
-		
+					
 		operations.addMouseListener(new MouseAdapter ()
 		{
 			@Override
@@ -146,7 +147,7 @@ public class AccueilConnecte {
 				mesOperations.setVisible(true);
 			}
 		});
-		
+					
 		frame.pack();
 		frame.setVisible(true);
 	}
