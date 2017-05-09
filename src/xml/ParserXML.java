491ed9e9edad98715ea54ab69f2d15aa460a4724
Chris <chrisjahim@free.fr>
2017-05-09 05:03:18 +0200

    Ajout de méthodes dans ParserXML

--- src/xml/ParserXML.java
+++ src/xml/ParserXML.java
@@ -1,5 +1,10 @@
 package xml;
 
+import donnees.Depense;
+import donnees.Evenement;
+import donnees.Message;
+import donnees.Utilisateur;
+
 public class ParserXML
 {
 	public static TypeRequete analyserType(String reception)
@@ -37,5 +42,44 @@ public class ParserXML
 		
 		
 		return TypeRequete.INCONNU;
+	}
+
+	public static Depense lireDepense(String reception)
+	{
+		//Ne marche pas mais donne la gueule du truc
+		Depense depense = new Depense();
+		ParserSAX p = new ParserSAX();
+		MySAXHandlerDepense handler = new MySAXHandlerDepense(depense);
+		p.monParsing(handler, reception);
+		return depense;
+	}
+
+	public static Message lireMessage(String reception)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static Utilisateur lireUtilisateur(String reception)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static Evenement lireEvenement(String reception)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 *  Va servir à enlever ce qui ne passerait pas au parser XML
+	 * @param reception
+	 * @return 
+	 */
+	public static String clean(String reception)
+	{
+		//On supprime "over" à la fin
+		return reception.substring(0, reception.length()-4);
 	}
 }
