6444b4667c53d89d72f9d2bb5ddddbb9b8b0934b
Chris <chrisjahim@free.fr>
2017-05-10 02:00:18 +0200

    HandlerSAX pour récupération d'une date dans demande

---
+++ src/xml/MySAXHandlerAnalyseDate.java
@@ -0,0 +1,50 @@
+package xml;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.helpers.DefaultHandler;
+
+import donnees.Depense;
+
+public class MySAXHandlerAnalyseDate extends DefaultHandler
+{
+	boolean copier = false;
+	String date;
+	
+	public MySAXHandlerAnalyseDate(String aCompleter)
+	{
+		date = aCompleter;
+	}
+	
+	public void startDocument()
+	{}
+	
+	public void endDocument()
+	{}
+	
+	public void startElement(String uri, String localName, String qName, Attributes attributes)
+	{
+		if(qName=="date")
+		{
+			copier = true;
+		}
+	}
+	
+	public void endElement(String uri, String localName, String qName)
+	{}
+	
+	public void characters(char[] ch, int start, int length)
+	{
+		if(copier)
+		{
+			String contenu = "";
+			
+			for(int i=start; i<start+length; i++)
+			{
+				contenu = contenu.concat(ch[i]+"");
+			}
+			
+			date = contenu;
+		}
+	}
+
+}
