ab4602635ccc6face9178e5938de89da867f8bda
Chris <chrisjahim@free.fr>
2017-05-10 20:12:55 +0200

    Ajout de test pour TestClient

--- src/clientExterne/TestClient.java
+++ src/clientExterne/TestClient.java
@@ -9,15 +9,18 @@ public class TestClient
 	public static void main(String[] args)
 	{
 		InteractionServeur iserv = new InteractionServeur();
-		
-		System.out.println(iserv.ajoutUtilisateur(new Utilisateur("nom","prenom","maill","pseudoo","mdp")));
-		
-		
-//		Evenement e = new Evenement("eventFromClientLourd",1000);
-//		System.out.println("Id de l'évenement avant: "+e.getId());
+//		
+//		Utilisateur u = new Utilisateur("nom","prenom","maill","pseudoo","mdp");
+//		System.out.println("Id de l'user avant: "+u.getId());
 //		System.out.println("Ajout");
-//		System.out.println(iserv.ajoutEvenement(e));
-//		System.out.println("Id de l'évenement après: "+e.getId());
+//		System.out.println(iserv.ajoutUtilisateur(u));
+//		System.out.println("Id de l'user après: "+u.getId());
+//		
+		Evenement e = new Evenement("eventFromClientLourd",1000);
+		System.out.println("Id de l'évenement avant: "+e.getId());
+		System.out.println("Ajout");
+		System.out.println(iserv.ajoutEvenement(e));
+		System.out.println("Id de l'évenement après: "+e.getId());
 	}
 
 }
