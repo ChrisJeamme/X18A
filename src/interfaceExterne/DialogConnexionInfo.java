8f68029fb7897c5040597985882ca9f2ddd422d8
TGranjon <thomas.granjon@orange.fr>
2017-05-10 15:57:53 +0200

    refactor: Correction de l'erreur sur interactionServeur

--- src/interfaceExterne/DialogConnexionInfo.java
+++ src/interfaceExterne/DialogConnexionInfo.java
@@ -1,7 +1,5 @@
 package interfaceExterne;
 
-import bdd.BDD;
-import bdd.InteractionBDD;
 import clientExterne.InteractionServeur;
 import donnees.Utilisateur;
 
@@ -11,25 +9,7 @@ public class DialogConnexionInfo {
 	  public DialogConnexionInfo(){}
 	  public DialogConnexionInfo(String pseudo, String mdp){
 		Utilisateur u = new Utilisateur();
-	    u.setPseudo(pseudo);
-	    u.setMotDePasse(mdp);
-	    
-	    /*BDD db = new BDD();
-
-		String redirection;
-		String message;
-		Utilisateur u = InteractionBDD.verificationConnexion(db, pseudo, mdp);
-		if (u != null) //L'utilisateur existe et s'est correctement authentifié
-		{
-			message = "Bonjour";
-		}
-		else 
-		{
-			message = "Utilisateur inconnu ou mot de passe incorrect.";
-		}
-		db.disconnect();*/
-	    
-	    //InteractionServeur.currentInteractionServeur.ajoutUtilisateur(u);
+	    u= InteractionServeur.currentInteractionServeur.connexion(pseudo, mdp);
 	  }
 
 	  public String toString(){
