ac057e7c3778bfb8f586253ceab3c32a86b77339
TGranjon <thomas.granjon@orange.fr>
2017-05-10 16:38:31 +0200

    refactor: Retrait des variables inutiles

--- src/interfaceExterne/DialogConnexionInfo.java
+++ src/interfaceExterne/DialogConnexionInfo.java
@@ -4,11 +4,11 @@ import clientExterne.InteractionServeur;
 import donnees.Utilisateur;
 
 public class DialogConnexionInfo {
-	  private String pseudo, mdp;
 
-	  public DialogConnexionInfo(){}
+	  public DialogConnexionInfo()
+	  {}
 	  public DialogConnexionInfo(String pseudo, String mdp){
 		Utilisateur u = new Utilisateur();
-	    u= InteractionServeur.currentInteractionServeur.connexion(pseudo, mdp);
+	    u = InteractionServeur.currentInteractionServeur.connexion(pseudo, mdp);
 	  }
 	}
