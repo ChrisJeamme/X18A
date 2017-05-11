d4e605300403dd4024d0b3d2e792d360285f6f1f
TGranjon <thomas.granjon@orange.fr>
2017-05-11 13:13:50 +0200

    feat: Indication des modifs à faire dès la résolution du probleme serveur

--- src/interfaceExterne/DialogConnexionInfo.java
+++ src/interfaceExterne/DialogConnexionInfo.java
@@ -8,5 +8,13 @@ public class DialogConnexionInfo {
 	  {}
 	  public DialogConnexionInfo(String pseudo, String mdp){
 	    AccueilNonConnecte.user = InteractionServeurStatic.currentInteractionServeur.connexion(pseudo, mdp);
+	    if(AccueilNonConnecte.user.getId()==-1) // La connection a-t-elle marchee ?
+	    {
+	    	// Erreur mettre un messsage et retourner sur la page non connectee
+	    }
+	    else 
+	    {
+	    	// C'est bon aller sur la page connectee avec le user en session
+	    }
 	  }
 	}
