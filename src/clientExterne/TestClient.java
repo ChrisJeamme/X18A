9aa7eb6de59f6bdfa28d602a77d11d149dc6b4e5
Chris <chrisjahim@free.fr>
2017-05-11 18:18:28 +0200

    Maj de la classe TestClient

--- src/clientExterne/TestClient.java
+++ src/clientExterne/TestClient.java
@@ -13,24 +13,26 @@ public class TestClient
 	public static void main(String[] args)
 	{
 		InteractionServeur iserv = new InteractionServeur();
-//		
+		
+		//Inscription
 //		Utilisateur u = new Utilisateur("nom","prenom","maill","pseudoo","mdp");
 //		System.out.println("Id de l'user avant: "+u.getId());
 //		System.out.println("Ajout");
 //		System.out.println(iserv.ajoutUtilisateur(u));
 //		System.out.println("Id de l'user après: "+u.getId());
-//		
+		
+		//Création d'un évenement
 //		Evenement e = new Evenement("eventFromClientLourd",1000);
 //		System.out.println("Id de l'évenement avant: "+e.getId());
 //		System.out.println("Ajout");
 //		System.out.println(iserv.ajoutEvenement(e));
 //		System.out.println("Id de l'évenement après: "+e.getId());
 		
-		Utilisateur u2;
-		System.out.println("Connexion");
-		u2 = iserv.connexion("cjeamme","pass");
-		System.out.println("Id de l'user: "+u2.getId());
-		
+		//Connexion
+//		Utilisateur u2;
+//		System.out.println("Connexion");
+//		u2 = iserv.connexion("cjeamme","zpass");
+//		System.out.println("Id de l'user: "+u2.getId());
 	}
 
 }
